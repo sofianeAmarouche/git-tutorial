c0b38e884dcee61735d63dc7998af135dc1a4a61
amarouche <azroubar@gmail.com>
2024-09-04 22:08:02 -0400

    Added git commands batch

---
+++ my-command.bat
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;\red27\green27\blue27;\red239\green239\blue239;}
+{\*\expandedcolortbl;;\cssrgb\c14118\c14118\c14118;\cssrgb\c94902\c94902\c94902;}
+\margl1440\margr1440\vieww33400\viewh18160\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
+git add .\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb3 git commit -m %1\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb3 git push}
